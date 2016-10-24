c38d44f9b9db57ef1343795e5fff8a90650d2a24
Steve Kondik <steve@cyngn.com>
2016-10-24 03:15:40 -0500

    Shamu : Enable sound trigger
    * For screen-off voice commands
    
    Change-Id: Idcb90f4eb9a1f50db0ab2810c56757219f372f66
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,7 @@ MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
 BOARD_EGL_CFG := device/moto/shamu/egl.cfg
 
 BOARD_USES_ALSA_AUDIO := true
+BOARD_SUPPORTS_SOUND_TRIGGER := true
 
 # Wifi related defines
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
