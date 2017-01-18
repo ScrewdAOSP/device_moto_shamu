d378264cb929e1a231defcda5ae95220f301578c
Roshan Pius <rpius@google.com>
2017-01-18 14:04:15 -0800

    Add wifi HAL service to device.mk
    Bug: 33460969
    Test: Compiles.
    Change-Id: Ic185f33e6f7e92f055f234b1ec17d42871012bad

--- device.mk
+++ device.mk
@@ -116,6 +116,7 @@ DEVICE_PACKAGE_OVERLAYS := \
     device/motorola/shamu/overlay
 
 PRODUCT_PACKAGES := \
+    android.hardware.wifi@1.0-service \
     libwpa_client \
     hostapd \
     wificond \
