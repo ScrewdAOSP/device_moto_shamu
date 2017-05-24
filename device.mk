d451ce2fb914b42ed565314ffc76dee1323943b2
Miao Wang <miaowang@google.com>
2017-05-24 15:34:15 -0700

    Addd RenderScript default implementation to Shamu device.mk
    Bug: 37035983
    
      - OVERRIDE_RS_DRIVER is moved from libRS_internal to the default
        implmentation. In order for GPU driver to work on legacy devices,
        the default implementation needs to be part of PRODUCT_PACKAGES.
    
    Test: mm
    Test: CTS tests pass on Fugu, and confirm GPU driver is working.
    Change-Id: I122c18be7a77449bf17c92d6b1c74c685433f249

--- device.mk
+++ device.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libstagefrighthw \
